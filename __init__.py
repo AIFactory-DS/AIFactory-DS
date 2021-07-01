e18ea6f6e0ba03ceae5063f16f6063e150aaf2b5
Sun Young Yoo <sunyoungyoo@Sunui-MacBookAir.local>
2021-07-02 00:09:10 +0900

    version 0.1.6

---
+++ __init__.py
@@ -0,0 +1,4 @@
+import os
+os.putenv('AIFactoryDS_PATH', os.path.abspath("./"))
+
+__all__ = ['EasterEggs', 'AIFactoryDS']
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+setup(  name='AIFactoryDS',
+
+        version='0.1.7',
+
+        description='Project Skeleton',
+
+        author='yoosunyoung',
+
+        author_email='luysunyoung9@gmail.com',
+
+        url='https://github.com/YooSunYoung',
+
+        license='MIT',
+
+        py_modules=['greetings', 'AbstractProcesses'],
+
+        python_requires='>=3.8',
+
+        install_requires=[],
+
+        packages=['EasterEggs', 'AIFactoryDS'],
+
+        scripts=['build_project_skeleton'],
+
+        include_package_data=True,
+
+        package_data={'training': ['formats/training_recipe_format.json']}
+)
